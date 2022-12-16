c76edf0a98ddd26f14807fc06fcb20e03c078164
AlexBNC01 <khalil.samigullin@gmail.com>
2022-12-16 21:33:11 +0300

    12

--- JS1/backg.js
+++ JS1/backg.js
@@ -14,5 +14,45 @@ function getRandomNumber() {
     return Math.floor(Math.random() * colors.length);
 }
 
+const inputText = document.querySelector("#input");
+const textBlock = document.querySelector("#text-block");
+const btn = document.querySelector("#btn")
+
+inputText.addEventListener("input", () => {
+        console.log(inputText.value);
+        textBlock.innerText = inputText.value;
+    }
+);
 
 
+
+
+
+
+
+
+
+
+
+
+
+/*
+const button1 = document.getElementById("btn1");
+const color1 = document.querySelector(".spcolor")
+
+const colors1 = ["#FFF", "rgb(57, 20, 20)", "rgb(57, 20, 60)", "grey", "yellow",
+];
+
+button.addEventListener("click", () => {
+    let hexColor = colors[getRandomNumber()]
+    document.body.style.backgroundColor = hexColor;
+    color.textContent = hexColor;
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
+
+
+
+*/
